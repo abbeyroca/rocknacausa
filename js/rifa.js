08f445eb2a5fe626f14631a034baf1d6dafbf6ea
Luisa Burini <lburini@unicamp.br>
2025-04-12 15:32:51 -0300

    :hankey: nome e id da planilha sao globais

--- js/rifa.js
+++ js/rifa.js
@@ -250,11 +250,13 @@ function createNewImage(i, hlink, name, grid, comprados) {
     grid.appendChild(icontainer);
 }
 
+const sheetId = "1k0_5lvTkX-u6FiG4jdNweVmse7kewzOH";
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;;
+const sheetName = "RIFA SOLIDARIA";
+
+
 function sorteio() {
     console.log("======================== PEGA NOMES ========================")
-    const sheetId = "1k0_5lvTkX-u6FiG4jdNweVmse7kewzOH";
-    const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;;
-    const sheetName = "RIFA SOLIDARIA";
     var url = `${base}&sheet=${sheetName}&tq=${encodeURIComponent("Select A, B")}`;
     var numeros = [];
     var nomes = [];
